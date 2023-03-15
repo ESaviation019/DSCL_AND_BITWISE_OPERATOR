392659bcff2e326012ee80711470cbb3e4f0cf72
Eshaan Sowale <96010356+ESaviation019@users.noreply.github.com>
2023-03-15 09:37:35 +0530

    Add files via upload

---
+++ AND.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Insert Number 1:  ");
+	scanf("%d",&x);
+	int y;
+	printf("Insert Number 2:  ");
+	scanf("%d",&y);
+	
+	printf("Output = %d", x & y);
+	
+return 0;
+}
+
+
